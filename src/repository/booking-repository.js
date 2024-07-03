55eb155d8e34f694964b6122a81542bd54cf9e01
Saurabh Singh <96985651+SaurabhSingh720@users.noreply.github.com>
2024-07-03 13:59:14 +0530

    added interservice communication using http

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -1,6 +1,6 @@
-const { ValidationError, AppError} = require('../utils/errors/index');
-const {Booking} = require('../models/index');
-const {StatusCodes} = require('http-status-codes');
+const { ValidationError, AppError } = require('../utils/errors/index');
+const { Booking } = require('../models/index');
+const { StatusCodes } = require('http-status-codes');
 
 class BookingRepository {
      async create(data) {
@@ -8,13 +8,30 @@ class BookingRepository {
                const booking = await Booking.create(data);
                return booking;
           } catch (error) {
-               if(error.name == 'SequelizeValidationError') {
+               if (error.name == 'SequelizeValidationError') {
                     throw new ValidationError(error);
                }
                throw new AppError(
                     'RepositoryError',
                     'Cannot create Booking',
                     'There was some issue with creating the booking, please try again later',
+                    StatusCodes.INTERNAL_SERVER_ERROR
+               );
+          }
+     }
+     async update(bookingId, data) {
+          try {
+               const booking = await Booking.findByPk(bookingId);
+               if(data.status) {
+                    booking.status = data.status;
+               }
+               await booking.save();
+               return booking;
+          } catch (error) {
+               throw new AppError(
+                    'RepositoryError',
+                    'Cannot update booking',
+                    'There was some issue with updating the booking, please try again later',
                     StatusCodes.INTERNAL_SERVER_ERROR
                );
           }
